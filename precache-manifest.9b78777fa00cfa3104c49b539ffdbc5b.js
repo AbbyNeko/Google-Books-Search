4c54a9d1b713d94422658019a2095cc9eb4d43c6
Abigail Limpioso <alimpioso@gmail.com>
2020-08-22 22:25:32 -0400

    Updates

---
+++ precache-manifest.9b78777fa00cfa3104c49b539ffdbc5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b1968830015f2000c5ec7a9f67776b6",
+    "url": "/AbbyNeko/Google-Books-Search/index.html"
+  },
+  {
+    "revision": "5fb102ed4e85e7d0128c",
+    "url": "/AbbyNeko/Google-Books-Search/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "475cc3981d3667f56910",
+    "url": "/AbbyNeko/Google-Books-Search/static/js/2.6e59459b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/AbbyNeko/Google-Books-Search/static/js/2.6e59459b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fb102ed4e85e7d0128c",
+    "url": "/AbbyNeko/Google-Books-Search/static/js/main.463b913c.chunk.js"
+  },
+  {
+    "revision": "4333a97643bfdcd91e11",
+    "url": "/AbbyNeko/Google-Books-Search/static/js/runtime-main.ead52675.js"
+  }
+]);
