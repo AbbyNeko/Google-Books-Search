8889895bdc9110c0c00c6e74051ffc9bb24601b0
Abigail Limpioso <alimpioso@gmail.com>
2020-08-23 00:09:24 -0400

    deployed and search works

--- routes/api/searchBooks.js
+++ routes/api/searchBooks.js
@@ -1,22 +1,23 @@
 const router = require("express").Router();
 const axios = require("axios");
 
+
+//searching for books based on keyword using Google books API
+function getGoogleBooks(req, res) {
+
+    axios.get("https://www.googleapis.com/books/v1/volumes?key=AIzaSyCmflHA1PwIhruMdmS8O8KE4C3VqkNIk8s&q="+req.params.keywords)
+        .then((response) => {
+            res.json(response.data.items);
+        })
+        .catch(error => {
+            console.log(`error - ${error}`);
+        });
+
+}
+
 // Matches with "/searchBooks/:keywords"
 router.route("/:keywords")
   .get(getGoogleBooks)
 
-
-//searching for books based on keyword using Google books API
-function getGoogleBooks(req, res, next) {
-
-    axios.get("https://www.googleapis.com/books/v1/volumes?key=AIzaSyCmflHA1PwIhruMdmS8O8KE4C3VqkNIk8s&q="+req.params.keywords)
-        .then((response) => {
-            res.json(response);
-        })
-        .catch(error => {
-            console.log(error);
-        });
-
-}
 
 module.exports = router;
