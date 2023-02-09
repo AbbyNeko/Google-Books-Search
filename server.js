61da091c6f95ce59e97cadbca090aa5db23b0519
Abigail Limpioso <alimpioso@gmail.com>
2023-02-08 20:11:23 -0500

    added dotenv code

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const routes = require("./routes");
 const mongoose = require("mongoose");
+require("dotenv").config();
 const db = require('./config/connection')
 
 const PORT = process.env.PORT || 3002;
